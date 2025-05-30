384f2767ce630b435f4dc4c65b3573c4354fc5ce
Addimator <adpri100@hhu.de>
2025-05-30 13:26:03 +0200

    Change focus dmr to only focus on exclusive dmrs

--- workflow/scripts/focus_dmrs.py
+++ workflow/scripts/focus_dmrs.py
@@ -6,34 +6,24 @@ sys.stderr = open(snakemake.log[0], "w")
 ecto = pybedtools.BedTool(snakemake.input["ecto"])
 endo = pybedtools.BedTool(snakemake.input["endo"])
 meso = pybedtools.BedTool(snakemake.input["meso"])
-all_files = [ecto, endo, meso]
 
-common_regions = ecto.intersect(endo).intersect(meso)
+ecto_only = ecto.intersect(endo, v=True).intersect(meso, v=True)
+endo_only = endo.intersect(ecto, v=True).intersect(meso, v=True)
+meso_only = meso.intersect(ecto, v=True).intersect(endo, v=True)
 
-# Remember regions to avoid duplicates
-seen = [set(), set(), set()]
+exclusive_sets = [
+    (ecto_only, snakemake.output["ecto"]),
+    (endo_only, snakemake.output["endo"]),
+    (meso_only, snakemake.output["meso"]),
+]
 
-with open(snakemake.output["ecto"], "w") as out_ecto, open(
-    snakemake.output["endo"], "w"
-) as out_endo, open(snakemake.output["meso"], "w") as out_meso:
+for regions, outfile in exclusive_sets:
+    with open(outfile, "w") as out:
+        for region in regions:
+            try:
+                value = float(region[4])
 
-    outputs = [out_ecto, out_endo, out_meso]
-
-    for region in common_regions:
-        region_bt = pybedtools.BedTool([region])
-
-        overlaps = [f.intersect(region_bt, wa=True) for f in all_files]
-
-        try:
-            meth_values = [float(overlap[0].fields[4]) for overlap in overlaps]
-        except (IndexError, ValueError):
-            continue
-
-        diff_meth = sum(abs(val) > 0.05 for val in meth_values)
-
-        if diff_meth in (1, 3):
-            for i, (match, seen_set, out) in enumerate(zip(overlaps, seen, outputs)):
-                line = str(match[0])
-                if line not in seen_set:
-                    print(line, file=out, end="")
-                    seen_set.add(line)
+                if abs(value) > 0.20:
+                    print(region, file=out, end="")
+            except (IndexError, ValueError):
+                continue
